12c50f7b45bf825c33521057b2cc117769e12bb3
festu-klimov <occidit3112@gmail.com>
2015-03-14 00:30:15 +1000

    Новый проект

---
+++ Telephony_selling/Telephony_selling/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Telephony_selling.MvcApplication" Language="C#" %>
---
+++ Telephony_selling/Telephony_selling/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Telephony_selling.Startup))]
+namespace Telephony_selling
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
